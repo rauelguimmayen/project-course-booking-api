59733282be4e78228cfd2268fddfb7166ed1a4ac
rauelguimmayen <rauelguimmayen@gmail.com>
2026-05-02 16:04:54 +0800

    updated auth.js

--- routes/enrollments.js
+++ routes/enrollments.js
@@ -1,13 +1,13 @@
 const express     = require("express");
 const router      = express.Router();
 const { requireAuth } = require("../middleware/auth");
-const enrollmentsController = require("../controllers/enrollments");
+const enrollmentsController = require("./app");
 
 
 // POST /api/courses/:id/enroll
-router.post("/:id/enroll", requireAuth, enrollmentsController.enrollCourse);
+router.post("/:id/enroll", requireAuth, enrollmentsController.enroll);
 
 // POST /api/courses/:id/unenroll
-router.post("/:id/unenroll", requireAuth, enrollmentsController.unenrollCourse);
+router.post("/:id/unenroll", requireAuth, enrollmentsController.unenroll);
 
 module.exports = router;
